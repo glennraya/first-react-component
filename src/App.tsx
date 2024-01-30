ea91ed5280ebd39a9e9cf3c47393153b911209b6
Glenn Raya <rayaglenn@gmail.com>
2024-01-30 20:09:36 +0800

    Initial commit - Creating first component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Message from './components/Message'
+
+function App() {
+    return (
+        <>
+            <h1>Hello React</h1>
+            <Message />
+        </>
+    )
+}
+
+export default App
